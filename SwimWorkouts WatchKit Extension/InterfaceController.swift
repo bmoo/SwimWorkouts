05f7b7199565b5b29c0a6ad73e2b145e05fca790
Brad Moore <moorebr@us.ibm.com>
2017-01-22 11:03:03 -0800

    Initial Commit

---
+++ SwimWorkouts WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SwimWorkouts WatchKit Extension
+//
+//  Created by Brad Moore on 1/22/17.
+//  Copyright © 2017 Brad Moore. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
